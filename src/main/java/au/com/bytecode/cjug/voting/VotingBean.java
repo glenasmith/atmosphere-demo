d6ddcef4b838d1ba10b2938a017a189940323031
Glen Smith <glen@bytecode.com.au>
2013-02-08 20:42:23 +1100

    More tweaks to try and make it happen but unlucky

--- src/main/java/au/com/bytecode/cjug/voting/VotingBean.java
+++ src/main/java/au/com/bytecode/cjug/voting/VotingBean.java
@@ -5,10 +5,16 @@
 package au.com.bytecode.cjug.voting;
 
 import java.io.Serializable;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import org.primefaces.push.PushContext;
 import org.primefaces.push.PushContextFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -18,20 +24,31 @@ import org.primefaces.push.PushContextFactory;
 @SessionScoped
 public class VotingBean implements Serializable {
     
-    private int count;
+	private static final Logger log = LoggerFactory.getLogger(VotingBean.class);
+	
+	private static final long serialVersionUID = 1L;
+	
+	private AtomicInteger count = new AtomicInteger();
  
     public int getCount() {
-        return count;
+        return count.get();
     }
  
     public void setCount(int count) {
-        this.count = count;
+        this.count.set(count);
+    }
+    
+    public void incomingMessage() {
+    	log.warn("Incoming message!");
+    	Map<String,String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();  
+        //String globalRecIdWithPrefix = params.get("globalRecId");  
     }
  
-    public synchronized void increment() {
-        count++;
+    public void increment() {
+    	log.warn("Invoking increment");
+        int next = count.incrementAndGet();
         PushContext pushContext = PushContextFactory.getDefault().getPushContext();
-        pushContext.push("/counter", String.valueOf(count));
+        pushContext.push("/counter", String.format("%d", next));
     }
     
 }
