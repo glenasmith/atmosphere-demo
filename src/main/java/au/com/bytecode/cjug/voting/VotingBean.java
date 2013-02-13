da2fbc2f05334bdd5230535ce94402c1a3a1541e
Glen Smith <glen@bytecode.com.au>
2013-02-13 14:15:14 +1100

    Tidied up css

--- src/main/java/au/com/bytecode/cjug/voting/VotingBean.java
+++ src/main/java/au/com/bytecode/cjug/voting/VotingBean.java
@@ -1,32 +1,20 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package au.com.bytecode.cjug.voting;
 
-import java.io.Serializable;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
-
-import javax.enterprise.context.SessionScoped;
 import javax.enterprise.inject.Model;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
-import javax.inject.Named;
+
 import org.primefaces.push.PushContext;
 import org.primefaces.push.PushContextFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
- *
+ * Simple chat backing bean using Atmosphere. 
+ * 
  * @author Glen
  */
 @Model
-public class VotingBean implements Serializable {
+public class VotingBean {
 
-    private static final Logger log = LoggerFactory.getLogger(VotingBean.class);
-    private static final long serialVersionUID = 1L;
     private String nextMessage;
 
     public String getNextMessage() {
@@ -39,8 +27,11 @@ public class VotingBean implements Serializable {
 
     public void sendChat() {
 
+    	FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Sent chat: " + nextMessage));
+    	
         PushContext pushContext = PushContextFactory.getDefault().getPushContext();
         pushContext.push("/chat", nextMessage);
-        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Sent chat: " + nextMessage));
+        
+        
     }
 }
